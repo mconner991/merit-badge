7d7193ae84b9e2fce48f1de6e4d93cec94bf0677
mconner991 <54862604+mconner991@users.noreply.github.com>
2020-01-23 13:56:42 -0500

    Create simple.js

---
+++ nim/simple.js
@@ -0,0 +1,15 @@
+/* Nim Super Duper Simple  */
+// declare variables for count (integer) and winner (string) as 0 and null. 
+// set a loop to run until someone loses (counts 21 or over).
+// the computer's turn is a random integer between 1 and 3
+// add turn to count
+// alert the computer's turn and the current count
+// set the winner as "you" since, given the computer just went, you'd be the winner if game over. 
+// if the computer didn't just count 21, do more stuff
+// set turn equal to a prompt ("Count how many? 1-3") and parse that input as an integer
+// add turn to count
+// say the current count
+// set the winner as "CPU" (since, if you just counted to 21, computer would win. 
+// close the conditional
+// close the loop
+// alert that game is over and say who won. 
