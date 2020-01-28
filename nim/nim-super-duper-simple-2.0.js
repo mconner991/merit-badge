56b531c0cb3e676e4d263e62f54482abf06a63db
mconner991 <54862604+mconner991@users.noreply.github.com>
2020-01-28 12:23:53 -0500

    Update nim-super-duper-simple-2.0.js

--- nim/nim-super-duper-simple-2.0.js
+++ nim/nim-super-duper-simple-2.0.js
@@ -8,21 +8,22 @@ while (count < 21){
 	// add turn to count
 	count += turn;
 	// alert the computer's turn and the current count
-	alert("computer counts "+turn+" and the count is now "+count+);
+	alert("computer counts "+turn+" and the count is now "+count);
 	// set the winner as "you" since, given the computer just went, you'd be the winner if game over. 
 	if (count >=21) winner = "you";
 // if the computer didn't just count 21, do more stuff
 	else { 
 // set turn equal to a prompt ("Count how many? 1-3") and parse that input as an integer
-		turn = prompt("count how many? 1-3");
+		turn =parseInt(prompt("count how many? 1-3"));
 // add turn to count 
 		count += turn;
 // say the current count
-		alert ("count is now +count+")
+		alert("count is now "+count);
 // set the winner as "CPU" (since, if you just counted to 21, computer would win. 
-		winner = cumputer
+		winner = "computer";
 // close the conditional
 	}
 // close the loop
 }
 // alert that game is over and say who won. 
+alert(winner+" won");
